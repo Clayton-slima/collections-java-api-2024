a23c35d28659b9879705d6dd621f570aab9c1b89
Clayton Lima <clayton-sl@hotmail.com>
2024-07-17 01:35:52 -0300

    Resolvendo exercicios ConjuntoConvidado, CadastroProdutos e AgendaContatos
    Signed-off-by: Clayton Lima <clayton-sl@hotmail.com>

---
+++ src/set/Pesquisa/Contato.java
@@ -0,0 +1,41 @@
+package set.Pesquisa;
+
+import java.util.Objects;
+
+public class Contato {
+    private String nome;
+    private int numero;
+
+    public Contato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(getNome(), contato.getNome());
+    }
+
+
+    public int hashCode() {
+        return Objects.hashCode(getNome());
+    }
+
+    public String toString() {
+        return "{" + nome + "," + numero + "}";
+    }
+}
